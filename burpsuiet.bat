6de9fa9a6cccfb6b634b048aaa584bd5e190054d
Dejavu666 <apps.oday@gmail.com>
2020-12-06 21:57:59 +0700

    Rename burpsuiet.bat to burpsuite.bat

--- burpsuiet.bat
+++
@@ -1,1 +0,0 @@
-java -javaagent:BurpSuiteLoader_v2020.11.jar -noverify -jar burpsuite_pro_v2020.11.jar
---
+++ burpsuite.bat
@@ -0,0 +1,1 @@
+java -javaagent:BurpSuiteLoader_v2020.11.jar -noverify -jar burpsuite_pro_v2020.11.jar
